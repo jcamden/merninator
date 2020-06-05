60fd243fb8cbc00b7243b2dda6a047e9e8a6a0a8
jcamden <john.camden@gmail.com>
2020-06-05 11:56:28 -0700

    chalked up Djinndex console logo

--- packages/server/src/utils/consoleLogo.ts
+++ packages/server/src/utils/consoleLogo.ts
@@ -5,7 +5,7 @@ export default function consoleLogo() {
   console.log(chalk.blue(`                   /// `));
   console.log(chalk.yellow(`           +       `) + chalk.blue(`\\\\\\  `));
   console.log(chalk.yellow(`      +         +   `) + chalk.blue(`||  `));
-  console.log(chalk.yellow(`         _-|-_     _//  `));
+  console.log(chalk.yellow(`         _-|-_     _` + chalk.blue(`//  `)));
   console.log(chalk.yellow(`    .-._/     \\___/ / `));
   console.log(chalk.yellow(`    \\ | ` + chalk.blue(`Djinndex`) + ` __/   `));
   console.log(chalk.yellow(`     ~'\\________/    `));
